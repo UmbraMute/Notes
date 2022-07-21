60ff08ce3d66423f128fcd5b03682f5b5e8971c1
CrowN3v3r <77667258+CrowN3v3r@users.noreply.github.com>
2022-07-21 14:17:19 +0500

    Добавьте файлы проекта.

---
+++ Note.WebApi/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Note.WebApi.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title cant be null or empty")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description cant be null or empty")]
+        public string Description { get; set; }
+    }
+}
---
+++ Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title cant be null or empty")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage ="Description cant be null or empty")]
+        public string Description { get; set; }
+    }
+}
